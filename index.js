84e20c7379f528c4e6328e724d7247af03af31fe
IsaacGHoward <howardisaac100@gmail.com>
2021-02-18 18:25:11 -0800

    Used env port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require your server and launch it here
 const server = require('./api/server');
 
-const port = 5000;
+const port = process.env.PORT;
 
 server.listen(port, () => {
   console.log("Server started @ localhost:5000")
